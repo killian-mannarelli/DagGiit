97f948eec41c52ca60cb5f4810d39c77c926fefb
Killian <kmannarelli@ensc.fr>
2024-12-13 14:50:04 +0000

    Fixing path

--- dags/run_main_dag.py
+++ dags/run_main_dag.py
@@ -13,13 +13,17 @@ default_args = {
 with DAG(
     'run_main_dag',
     default_args=default_args,
-    description='Run main.py with virtual environment and dependencies',
+    description='Run main.py with virtual environment and credentials',
     schedule_interval=None,  # Manual trigger
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
 
-    run_main_with_venv = BashOperator(
-        task_id='run_main_script_with_dependencies',
-        bash_command='pip install kafka-python google-cloud-bigquery && python /opt/airflow/dags/repo/scripts/main.py',
+    run_main_with_credentials = BashOperator(
+        task_id='run_main_script_with_credentials',
+        bash_command=(
+            'pip install kafka-python google-cloud-bigquery && '
+            'export GOOGLE_APPLICATION_CREDENTIALS=/opt/airflow/credentials/service-account.json && '
+            'python /opt/airflow/dags/repo/scripts/main.py'
+        ),
     )
