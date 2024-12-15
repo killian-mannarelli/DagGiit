5d588001e488119ed2f1e585e37d44229df3e37a
Killian <kmannarelli@ensc.fr>
2024-12-15 14:30:41 +0000

    Fixing path

--- dags/run_main_dag.py
+++ dags/run_main_dag.py
@@ -19,24 +19,15 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    # Task to write the GCP service account secret to a file
-    create_secret_file = BashOperator(
-        task_id='create_service_account_file',
-        bash_command=(
-            'echo "{{ var.value.GCP_SERVICE_ACCOUNT }}" | base64 -d > /tmp/service-account.json && '
-            'echo "Secret file created at /tmp/service-account.json"'
-        )
-    )
 
     # Task to run the main script with the secret file
     run_main_with_secret = BashOperator(
         task_id='run_main_script_with_dependencies',
         bash_command=(
+            'echo "{{ var.value.GCP_SERVICE_ACCOUNT }}" | base64 -d > /tmp/service-account.json && '
+            'echo "Secret file created at /tmp/service-account.json" &&'
             'pip install kafka-python google-cloud-bigquery && '
             'export GOOGLE_APPLICATION_CREDENTIALS=/tmp/service-account.json && '
             'python /opt/airflow/dags/repo/scripts/main.py'
         )
     )
-
-    # Set task dependencies
-    create_secret_file >> run_main_with_secret
