5bbdd418b49541b76ce10b04f37590e4b2275ec5
Killian <kmannarelli@ensc.fr>
2024-12-15 15:46:51 +0000

    Fixing path

--- dags/run_main_dag.py
+++ dags/run_main_dag.py
@@ -13,21 +13,19 @@ default_args = {
 with DAG(
     'run_main_dag',
     default_args=default_args,
-    description='Run main.py with virtual environment and GCP secret file',
+    description='Run main.py with dynamic Kafka host',
     schedule_interval=None,  # Manual trigger
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
-
 
     # Task to run the main script with the secret file
     run_main_with_secret = BashOperator(
         task_id='run_main_script_with_dependencies',
         bash_command=(
             'echo "{{ var.value.GCP_SERVICE_ACCOUNT }}" | base64 -d > /tmp/service-account.json && '
-            'echo "Secret file created at /tmp/service-account.json" &&'
-            'pip install kafka-python google-cloud-bigquery && '
-            'export GOOGLE_APPLICATION_CREDENTIALS=/tmp/service-account.json && '
-            'python /opt/airflow/dags/repo/scripts/main.py'
+            'echo "Secret file created at /tmp/service-account.json" && '
+            'pip install kafka-python && '
+            'python /opt/airflow/dags/repo/scripts/main.py --kafka_host "{{ dag_run.conf["kafka_host"] }}"'
         )
     )
