bf8a41e9acead4456ca79c1b8b22eba803a80b5f
Killian <kmannarelli@ensc.fr>
2024-12-15 16:01:55 +0000

    Fixing path

--- dags/run_main_dag.py
+++ dags/run_main_dag.py
@@ -13,19 +13,17 @@ default_args = {
 with DAG(
     'run_main_dag',
     default_args=default_args,
-    description='Run main.py with dynamic Kafka host',
+    description='Run main.py with Kafka host from secret',
     schedule_interval=None,  # Manual trigger
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
 
-    # Task to run the main script with the secret file
+    # Task to run the main script with the Kafka host from Airflow variables
     run_main_with_secret = BashOperator(
-        task_id='run_main_script_with_dependencies',
+        task_id='run_main_script_with_secret',
         bash_command=(
-            'echo "{{ var.value.GCP_SERVICE_ACCOUNT }}" | base64 -d > /tmp/service-account.json && '
-            'echo "Secret file created at /tmp/service-account.json" && '
             'pip install kafka-python && '
-            'python /opt/airflow/dags/repo/scripts/main.py --kafka_host "{{ dag_run.conf["kafka_host"] }}"'
+            'python /opt/airflow/dags/repo/scripts/main.py --kafka_host "{{ var.value.KAFKA_HOST }}"'
         )
     )
